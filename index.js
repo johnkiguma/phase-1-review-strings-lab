8bc208f033604e68d71777e196f3c8faea019d5d
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-16 21:04:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const currentUser = 'john kiguma';
-const welcomeMessage = 'Welcome to Flatbook, + ${johnkiguma}!';
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-currentUser.slice(0, 1);
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
 
