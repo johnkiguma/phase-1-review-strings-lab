89dfc95b775a59f034cc5ab492db52e1ea51eb86
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-16 11:39:18 +0300

    completed assignnment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'john kiguma';
+const welcomeMessage = 'Welcome to Flatbook, + ${johnkiguma}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
